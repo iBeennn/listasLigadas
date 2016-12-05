90501bdb9ccf1de4340edef5e0898d7fb0fbe2a9
iBeennn <beatles_ben@hotmail.com>
2016-12-04 18:19:25 -0600

    Update usaPila.c
    comentarios corregidos

--- usaPila.c
+++ usaPila.c
@@ -1,8 +1,7 @@
 /*
-  Programa para probar el funcionamiento de las listas ligadas
-  de enteros definidas en el archivo listaLigada.c
+  Programa para probar el funcionamiento de las pilas
+  de enteros definidas en el archivo pila.c
 
-  Marco Antonio Heredia Velasco
 */
 #include <stdio.h>
 #include "pila.h"
@@ -12,35 +11,35 @@ void main()
    int i;
    pila p;
 
-   //inicializamos la lista l para usarla
+   //inicializamos la pila l para usarla
    inicializa(&p);
 
-   //agregamos elementos al la lista
+   //agregamos elementos a la pila
    push(&p, 12);
    push(&p, 4);
    push(&p, 5);
    push(&p, 3);
 
-   // ahora la lista es 12, 4, 5, 3
+   // ahora la pila es 12, 4, 5, 3
 
-   //insertamos un 90 en el indice 2 de la lista
+   //insertamos un 90 en la pila
    push(&p, 90);
 
-   // ahora la lista es 12, 4, 90, 5, 3
+   // ahora la pila es 12, 4, 5, 3, 90
 
-   //borramos el elemento con indice 1
+   //borramos el elemento 90
    pop(&p);
 
-   // ahora la lista es 12, 90, 5, 3
+   // ahora la lista es 12, 4, 5, 3
 
 
    if (esVacia(&p))
-      printf("\nLa lista es vacia.\n");
+      printf("\nLa pila es vacia.\n");
    else
-      printf("\nLa lista NO es vacia.\n");
+      printf("\nLa pila NO es vacia.\n");
 
-   printf("Longitud de la lista: %d\n", longitud(&p));
-   printf("Los elementos dentro la lista son:\n");
+   printf("Longitud de la pila: %d\n", longitud(&p));
+   printf("Los elementos dentro la pila son:\n");
 
    for (i=0; i< longitud(&p); i++){
       printf("%d\n", tope(&p));
